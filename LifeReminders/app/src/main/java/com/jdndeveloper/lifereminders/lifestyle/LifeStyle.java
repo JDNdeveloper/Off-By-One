b1d643e906fbc164f744a1e43d743b7219951dea
Kevin Cheng <keccheng@ucsc.edu>
2015-01-29 22:20:03 -0800

    Initial LifeStyle addition
    Everything should be now working except for the addReminder and
    deleteReminder as I am waiting for the storage class to finalize.

--- LifeReminders/app/src/main/java/com/jdndeveloper/lifereminders/lifestyle/LifeStyle.java
+++ LifeReminders/app/src/main/java/com/jdndeveloper/lifereminders/lifestyle/LifeStyle.java
@@ -1,50 +1,109 @@
 package com.jdndeveloper.lifereminders.lifestyle;
 
+import android.provider.CalendarContract;
+
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by jgemig on 1/27/2015.
+ * Created by Kevin Cheng on 1/27/2015.
  */
 public class LifeStyle {
 
-    private String lifeStyleKey = "lifeStyle_default_key";
-    private String lifeStyleName = "life style default name";
-    private boolean enabled = false;
-    private List<String> lifeStyleNotifications;
-
-    public LifeStyle (){
+    private String name = "";
+    private String key = "";
+    boolean enabled = false;
+/* *
+ * Lifestyle Name: The following contains the setting and returning the name of the lifestyle.
+ * Values used: String
+ * Variables used: name
+ */
+    /* *
+     * getKey(): Function that will return the value of name
+     * Arguments: none
+     * Return:
+     *      * Type: String
+     *      * Item: The key of the lifestyle
+     */
+    public String getKey() {
+        return this.key;
+    }
+    /* *
+     * setKey(): Function that will set the name of the lifestyle
+     * Arguments:
+     *       * Type: String
+     *       * Item: key of the Lifestyle
+     * Return: none
+     */
+    public void setKey(String inputKey) {
+        this.key = inputKey;
     }
 
-    public String getLifeStyleKey() {
-        return lifeStyleKey;
+    /* *
+         * getName(): Function that will return the value of name
+         * Arguments: none
+         * Return:
+         *      * Type: String
+         *      * Item: The name of the lifestyle
+         */
+    public String getName() {
+        return this.name;
     }
-
-    public void setLifeStyleKey(String lifeStyleKey) {
-        this.lifeStyleKey = lifeStyleKey;
+    /* *
+         * getName(): Function that will return the value of name
+         * Arguments: none
+         * Return:
+         *      * Type: String
+         *      * Item: The key of the lifestyle
+         */
+    public void setName(String inputName) {
+        this.name = inputName;
     }
-
-    public String getLifeStyleName() {
-        return lifeStyleName;
+    /* removeReminder(): Function that will remove the reminder from the lifestyle
+            * Arguments: Reminder ID. The ID of the reminder
+            * Return: none
+            */
+    public void addReminder(String reminderName) {
+        //Create a new Reminder
+        Reminder newReminder = getNew<Reminder>();
+        commit<Reminder>(newReminder);
+        //Set the reminder
+        newReminder.setName(reminderName);
+        newReminder.setEnabled();
+        //Set the
+        commit<Reminder>(newReminder);
     }
+    /* removeReminder(): Function that will remove the reminder from the lifestyle
+        * Arguments: Reminder ID. The ID of the reminder
+        * Return: none
+        */
+    public void removeReminder(String reminderID) {
+        //Get the reminder object from the reminderID
+        Reminder object =  getReminder(reminderID);
+        //Disable it just in case
+        object.setDisable();
+        //Set it to null so that it points to nothing now so that
+        object = null;
+        //Garbage Collector will clean it up for us
+        //Remove object in list
 
-    public void setLifeStyleName(String lifeStyleName) {
-        this.lifeStyleName = lifeStyleName;
     }
-
-    public boolean isEnabled() {
-        return enabled;
+    /* getState(): Function that will return the state of the lifestyle
+    * Arguments: none
+    * Return:
+    *      * Type: boolean
+    *      * Item: Enabled/Disabled (Disabled by default)
+    */
+    public boolean getState() {
+        return this.enabled;
     }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
-
-    public List<String> getLifeStyleNotifications() {
-        return lifeStyleNotifications;
-    }
-
-    public void setLifeStyleNotifications(List<String> lifeStyleNotifications) {
-        this.lifeStyleNotifications = lifeStyleNotifications;
+    /* getState(): Function that will return the state of the lifestyle
+        * Arguments:
+        *      * Type: boolean
+        *      * Item: Enabled/Disabled
+        * Return: none
+        */
+    public void setState(boolean state) {
+        this.enabled = state;
     }
 }
