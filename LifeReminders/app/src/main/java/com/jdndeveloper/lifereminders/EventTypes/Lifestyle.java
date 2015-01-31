61c4688917f375bd2afd01a6bd1f9efe6bce1243
Kevin Cheng <keccheng@ucsc.edu>
2015-01-31 00:40:43 -0800

    Changed Super Calls

--- LifeReminders/app/src/main/java/com/jdndeveloper/lifereminders/EventTypes/Lifestyle.java
+++ LifeReminders/app/src/main/java/com/jdndeveloper/lifereminders/EventTypes/Lifestyle.java
@@ -9,20 +9,18 @@ import java.util.List;
  */
 public class Lifestyle extends AbstractBaseEvent {
 
-    private List<String> notifications;
+    private List<String> lifestyleReminders;
 
-    private String name = "DEFAULT LIFESTYLE NAME"; //notification doesn't use this
-    private String key = "LIFESTYLE_DEFAULT_KEY";
-    private boolean enabled = true;
-
-    public Lifestyle() {}
-
-    public List<String> getLifestyleReminders() {
-        return notifications;
+    public Lifestyle() {
+        super("DEFAULT LIFESTYLE NAME", "DEFAULT_LIFESTYLE_KEY", false);
     }
 
-    public void setLifestyleReminders(List<String> lifestyleNotifications) {
-        this.notifications = lifestyleNotifications;
+    public List<String> getLifestyleReminders() {
+        return lifestyleReminders;
+    }
+
+    public void setLifestyleReminders(List<String> lifestyleNotificationsArg) {
+        this.lifestyleReminders = lifestyleNotificationsArg;
     }
 
     /* addReminder(): Function that will add the key to the new reminder
@@ -30,7 +28,8 @@ public class Lifestyle extends AbstractBaseEvent {
             * Return: none
             */
     public void addReminder(String reminderKey) {
-        notifications.add(reminderKey);
+        lifestyleReminders.add(reminderKey);
+        return;
     }
     /* removeReminder(): Function that will remove the reminder ID from the lifestyleNotifications
         * Arguments: Reminder ID. The ID of the reminder
@@ -38,12 +37,12 @@ public class Lifestyle extends AbstractBaseEvent {
         */
     public void removeReminder(String reminderID) {
         //Scan through the arraylist
-        for(int i = 0; i < this.notifications.size(); i++ ){
+        for(int i = 0; i < this.lifestyleReminders.size(); i++ ){
             //Check if the ID is in that item in Arraylist
-            if (this.notifications.get(i).equals(reminderID)) {
+            if (this.lifestyleReminders.get(i).equals(reminderID)) {
                 //If it is fond, remove the item in Arraylist and just get out of the function.
                 //The rest outside the for loop is some error debugging info.
-                this.notifications.remove(i);
+                this.lifestyleReminders.remove(i);
                 return;
             }
         }
@@ -52,8 +51,9 @@ public class Lifestyle extends AbstractBaseEvent {
                 "lifestyleNotifications does not have reminderID\nredminerID = %s\n", reminderID);
         //Print contents in arraylist
         System.err.println("Lifestyle Notifications Arraylist");
-        for (int j = 0; j < this.notifications.size(); j++) {
-            System.err.printf("%d: %s\n", j, notifications.get(j));
+        for (int j = 0; j < this.lifestyleReminders.size(); j++) {
+            System.err.printf("%d: %s\n", j, lifestyleReminders.get(j));
         }
+        return;
     }
 }
