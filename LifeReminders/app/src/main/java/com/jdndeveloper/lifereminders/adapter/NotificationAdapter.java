e822e0e866fcb8ceaabf8c120b2be549dd8de942
Jayden Navarro <jdndeveloper@gmail.com>
2015-02-21 20:56:14 -0800

    Updated Notification Adapter

--- LifeReminders/app/src/main/java/com/jdndeveloper/lifereminders/adapter/NotificationAdapter.java
+++ LifeReminders/app/src/main/java/com/jdndeveloper/lifereminders/adapter/NotificationAdapter.java
@@ -1,16 +1,22 @@
 package com.jdndeveloper.lifereminders.adapter;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jdndeveloper.lifereminders.EventTypes.Notification;
 import com.jdndeveloper.lifereminders.EventTypes.Reminder;
 import com.jdndeveloper.lifereminders.R;
+import com.jdndeveloper.lifereminders.storage.Storage;
 
+import java.util.Calendar;
 import java.util.List;
 
 /**
@@ -31,16 +37,54 @@ public class NotificationAdapter extends ArrayAdapter{
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         if (convertView == null)
             convertView = inflater.inflate(rowResId, parent, false);
 
-        TextView rowTextView1 = (TextView) convertView.findViewById(R.id.rowTextView1);
-        TextView rowTextView2 = (TextView) convertView.findViewById(R.id.rowTextView2);
-        TextView rowTextView3 = (TextView) convertView.findViewById(R.id.rowTextView3);
-        rowTextView1.setText(notifications.get(position).getName());
-        rowTextView2.setText(notifications.get(position).getKey());
-        rowTextView3.setText(Boolean.toString(notifications.get(position).isEnabled()));
+        TextView rowTextView = (TextView) convertView.findViewById(R.id.rowNotificationTextView);
+        TextView containerReminder = (TextView) convertView.findViewById(R.id.rowNotificationContainerReminder);
+        Switch theSwitch = (Switch) convertView.findViewById(R.id.enabledSwitchNotificationRow);
+
+        // You need to null out the listener before you change the state of the checkbox. Otherwise,
+        // the previous listener will be fired off. Likewise, all fields must be repopulated because
+        // convertView isn't guaranteed to be clean - john - just an fyi
+
+        theSwitch.setOnCheckedChangeListener(null);
+
+        Calendar time = notifications.get(position).getTime();
+
+        String amPM = "amPM FAIL";
+
+        if (time.get(Calendar.AM_PM) == 1) {
+            amPM = "PM";
+        } else {
+            amPM = "AM";
+        }
+
+        rowTextView.setText(Integer.toString(time.get(Calendar.HOUR))
+                + ":" + Integer.toString(time.get(Calendar.MINUTE))
+                + " " + amPM);
+
+        String reminderKey = notifications.get(position).getReminderContainerKey();
+
+        containerReminder.setText(Storage.getInstance().getReminder(reminderKey).getName());
+        //containerReminder.setText("TEST 123");
+
+        theSwitch.setChecked(notifications.get(position).isEnabled());
+
+        theSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                //change enabled state of Lifestyle
+                String text = notifications.get(position).getName();
+                if (isChecked) {
+                    text += " is enabled";
+                } else {
+                    text += " is disabled";
+                }
+                Toast.makeText(getContext(), text, Toast.LENGTH_SHORT).show();
+            }
+        });
+
         return convertView;
     }
     @Override
