c8a1d721e20cfaa3f1a5a4981eecd95f52795264
Jayden Navarro <jdndeveloper@gmail.com>
2015-03-09 11:55:09 -0700

    Notifications send at beginning of minute instead of end, Notifications get set on boot-up properly

--- LifeReminders/app/src/main/java/com/jdndeveloper/lifereminders/EventTypes/Notification.java
+++ LifeReminders/app/src/main/java/com/jdndeveloper/lifereminders/EventTypes/Notification.java
@@ -50,7 +50,7 @@ public class Notification extends AbstractBaseEvent {
         repeatEveryBlankDaysEnabled = false;
 
         Calendar newCalendar = Calendar.getInstance();
-        newCalendar.set(Calendar.SECOND, 59);
+        newCalendar.set(Calendar.SECOND, 0);
         newCalendar.set(Calendar.MINUTE, 59);
         newCalendar.set(Calendar.HOUR_OF_DAY, 23);
         newCalendar.set(Calendar.DAY_OF_MONTH, 1);
