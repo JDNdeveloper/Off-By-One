9ee949ddb785040f5d9eac9a9855074e304c1ae6
Jayden Navarro <jdndeveloper@gmail.com>
2015-03-14 22:34:04 -0700

    Refactoring AlarmProcedure test code

--- AlarmProcedures/tests/alarmtests/AlarmManagerTester.java
+++ AlarmProcedures/tests/alarmtests/AlarmManagerTester.java
@@ -16,14 +16,16 @@ public class AlarmManagerTester {
 
     private final Random random = new Random();
 
+    private enum Procedure {SET_REPEAT_DAYS, SET_REPEAT_BLANK, SETUP_REPEAT_DAYS, SETUP_REPEAT_BLANK, MAKE_REPEAT_DAYS, MAKE_REPEAT_BLANK}
+
+    private enum TestEnum {HELLO, HI, HOLA}
+
     @Test
     public void setAlarmRepeatDays() {
         System.out.println(TAG + "testing setAlarm()");
 
         Calendar rightNow = Calendar.getInstance();
         Calendar correctCalendar = Calendar.getInstance();
-
-
 
         AlarmManager alarmManager = new AlarmManager();
 
@@ -115,15 +117,6 @@ public class AlarmManagerTester {
 
 
 
-    }
-
-    public int randInt(int min, int max) {
-
-        // nextInt is normally exclusive of the top value,
-        // so add 1 to make it inclusive
-        int randomNum = random.nextInt((max - min) + 1) + min;
-
-        return randomNum;
     }
 
     @Test
@@ -282,8 +275,183 @@ public class AlarmManagerTester {
         }
     }
 
+    @Test
+    public void calendarTester() {
 
-    //Above two tests do the same thing
+        assertEquals(TimeUnit.MILLISECONDS.convert(1, TimeUnit.DAYS)*3, TimeUnit.MILLISECONDS.convert(3, TimeUnit.DAYS));
+
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.set(Calendar.DAY_OF_YEAR, 73);
+
+        calendar.set(Calendar.YEAR, 2015);
+
+        int properDayOfWeek = Calendar.TUESDAY;
+        int properDate = 29;
+        int properYear = 2042;
+        int properMonth = Calendar.JULY;
+
+        for (int day = 0; day < 9999; day++) {
+            calendar.add(Calendar.DATE, 1);
+        }
+
+        System.out.println(calendar);
+
+        assertEquals(TAG + "Year: ",
+                properYear, calendar.get(Calendar.YEAR));
+
+        assertEquals(TAG + "Month: ",
+                properMonth, calendar.get(Calendar.MONTH));
+
+        assertEquals(TAG + "Date: ",
+                properDate, calendar.get(Calendar.DATE));
+
+        assertEquals(TAG + "Day of Week: ",
+                properDayOfWeek, calendar.get(Calendar.DAY_OF_WEEK));
+    }
+
+    @Test
+    public void enumTester() {
+        TestEnum testEnum = TestEnum.HOLA;
+        assertTrue("HOLA not called properly", switchEnumTest(testEnum) == 3);
+
+        testEnum = TestEnum.HI;
+        assertTrue("HI not called properly", switchEnumTest(testEnum) == 2);
+
+        testEnum = testEnum.HELLO;
+        assertTrue("HELLO not called properly", switchEnumTest(testEnum) == 1);
+    }
+
+    public int switchEnumTest(TestEnum properEnum) {
+        switch(properEnum) {
+            case HELLO:
+                return 1;
+            case HI:
+                return 2;
+            case HOLA:
+                return 3;
+            default:
+                return -1;
+        }
+    }
+
+
+    public void simulateDays(int numberOfDays, Procedure correctProcedure, AlarmManager alarmManager) {
+        //make Procedure enum type
+    }
+
+    public boolean runCorrectProcedure(Procedure correctProcedure, AlarmManager alarmManager, Calendar rightNow) {
+        switch (correctProcedure) {
+            case SET_REPEAT_DAYS:
+                alarmManager.setAlarm(rightNow);
+                break;
+            case SET_REPEAT_BLANK:
+
+                break;
+            case SETUP_REPEAT_DAYS:
+
+                break;
+            case SETUP_REPEAT_BLANK:
+
+                break;
+            case MAKE_REPEAT_DAYS:
+
+                break;
+            case MAKE_REPEAT_BLANK:
+
+                break;
+            default:
+                return false;
+        }
+
+        return true;
+    }
+
+
+    public int randInt(int min, int max) {
+
+        // nextInt is normally exclusive of the top value,
+        // so add 1 to make it inclusive
+        int randomNum = random.nextInt((max - min) + 1) + min;
+
+        return randomNum;
+    }
+
+    public boolean setToProperDay(Calendar calendar) {
+        switch (calendar.get(Calendar.DAY_OF_WEEK)) {
+            case Calendar.MONDAY:
+                calendar.add(Calendar.DATE, 1);
+                //calendar.set(Calendar.DAY_OF_WEEK, Calendar.TUESDAY);
+                break;
+            case Calendar.TUESDAY:
+                addDaysToCalendar(calendar, 3);
+                //calendar.add(Calendar.DATE, 3);
+                //calendar.set(Calendar.DAY_OF_WEEK, Calendar.FRIDAY);
+                break;
+            case Calendar.FRIDAY:
+                addDaysToCalendar(calendar, 3);
+                //calendar.add(Calendar.DATE, 3);
+                //calendar.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+                break;
+            default:
+                return false;
+        }
+
+        return true;
+    }
+
+    private void addDaysToCalendar(Calendar calendar, int numberOfDays) {
+        for (int i = 0; i < numberOfDays; i++) {
+            calendar.add(Calendar.DATE, 1);
+        }
+    }
+
+    public String buildCalendarString(Calendar expectedCalendar, Calendar actualCalendar) {
+        String calendarString = "\n\n";
+
+        calendarString += "Expected Date: " + expectedCalendar.get(Calendar.DATE) + "\n";
+        calendarString += "Actual Date: " + actualCalendar.get(Calendar.DATE) + "\n";
+
+        calendarString += "\n";
+
+        calendarString += "Expected Month: " + expectedCalendar.get(Calendar.MONTH) + "\n";
+        calendarString += "Actual Month: " + actualCalendar.get(Calendar.MONTH) + "\n";
+
+        calendarString += "\n";
+
+        calendarString += "Expected Year: " + expectedCalendar.get(Calendar.YEAR) + "\n";
+        calendarString += "Actual Year: " + actualCalendar.get(Calendar.YEAR) + "\n";
+
+        calendarString += "\n";
+
+        calendarString += "Expected Hour: " + expectedCalendar.get(Calendar.HOUR_OF_DAY) + "\n";
+        calendarString += "Actual Hour: " + actualCalendar.get(Calendar.HOUR_OF_DAY) + "\n";
+
+        calendarString += "\n";
+
+        calendarString += "Expected Minute: " + expectedCalendar.get(Calendar.MINUTE) + "\n";
+        calendarString += "Actual Minute: " + actualCalendar.get(Calendar.MINUTE) + "\n";
+
+        calendarString += "\n";
+
+        //calendarString += "Date: " + calendar.get(Calendar.DATE) + "\n";
+
+        return calendarString;
+    }
+
+    public Calendar getOffsetCalendar(Calendar originalCalendar, int lowerBound, int upperBound, int multiplier) {
+        Calendar offsetCalendar = (Calendar) originalCalendar.clone();
+
+        int randomDayOffset = randInt(lowerBound, upperBound);
+
+        addDaysToCalendar(offsetCalendar, randomDayOffset*multiplier);
+
+        return offsetCalendar;
+    }
+
+
+    public void TRASH() {
+        //Above two tests do the same thing
     /*
     @Test
     public void makeNextNotificationTime() {
@@ -346,113 +514,6 @@ public class AlarmManagerTester {
         }
     }
     */
-
-    public boolean setToProperDay(Calendar calendar) {
-        switch (calendar.get(Calendar.DAY_OF_WEEK)) {
-            case Calendar.MONDAY:
-                calendar.add(Calendar.DATE, 1);
-                //calendar.set(Calendar.DAY_OF_WEEK, Calendar.TUESDAY);
-                break;
-            case Calendar.TUESDAY:
-                addDaysToCalendar(calendar, 3);
-                //calendar.add(Calendar.DATE, 3);
-                //calendar.set(Calendar.DAY_OF_WEEK, Calendar.FRIDAY);
-                break;
-            case Calendar.FRIDAY:
-                addDaysToCalendar(calendar, 3);
-                //calendar.add(Calendar.DATE, 3);
-                //calendar.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
-                break;
-            default:
-                return false;
-        }
-
-        return true;
-    }
-
-    private void addDaysToCalendar(Calendar calendar, int numberOfDays) {
-        for (int i = 0; i < numberOfDays; i++) {
-            calendar.add(Calendar.DATE, 1);
-        }
-    }
-
-    @Test
-    public void calendarTester() {
-
-        assertEquals(TimeUnit.MILLISECONDS.convert(1, TimeUnit.DAYS)*3, TimeUnit.MILLISECONDS.convert(3, TimeUnit.DAYS));
-
-        Calendar calendar = Calendar.getInstance();
-
-        calendar.set(Calendar.DAY_OF_YEAR, 73);
-
-        calendar.set(Calendar.YEAR, 2015);
-
-        int properDayOfWeek = Calendar.TUESDAY;
-        int properDate = 29;
-        int properYear = 2042;
-        int properMonth = Calendar.JULY;
-
-        for (int day = 0; day < 9999; day++) {
-            calendar.add(Calendar.DATE, 1);
-        }
-
-        System.out.println(calendar);
-
-        assertEquals(TAG + "Year: ",
-                properYear, calendar.get(Calendar.YEAR));
-
-        assertEquals(TAG + "Month: ",
-                properMonth, calendar.get(Calendar.MONTH));
-
-        assertEquals(TAG + "Date: ",
-                properDate, calendar.get(Calendar.DATE));
-
-        assertEquals(TAG + "Day of Week: ",
-                properDayOfWeek, calendar.get(Calendar.DAY_OF_WEEK));
-    }
-
-    public String buildCalendarString(Calendar expectedCalendar, Calendar actualCalendar) {
-        String calendarString = "\n\n";
-
-        calendarString += "Expected Date: " + expectedCalendar.get(Calendar.DATE) + "\n";
-        calendarString += "Actual Date: " + actualCalendar.get(Calendar.DATE) + "\n";
-
-        calendarString += "\n";
-
-        calendarString += "Expected Month: " + expectedCalendar.get(Calendar.MONTH) + "\n";
-        calendarString += "Actual Month: " + actualCalendar.get(Calendar.MONTH) + "\n";
-
-        calendarString += "\n";
-
-        calendarString += "Expected Year: " + expectedCalendar.get(Calendar.YEAR) + "\n";
-        calendarString += "Actual Year: " + actualCalendar.get(Calendar.YEAR) + "\n";
-
-        calendarString += "\n";
-
-        calendarString += "Expected Hour: " + expectedCalendar.get(Calendar.HOUR_OF_DAY) + "\n";
-        calendarString += "Actual Hour: " + actualCalendar.get(Calendar.HOUR_OF_DAY) + "\n";
-
-        calendarString += "\n";
-
-        calendarString += "Expected Minute: " + expectedCalendar.get(Calendar.MINUTE) + "\n";
-        calendarString += "Actual Minute: " + actualCalendar.get(Calendar.MINUTE) + "\n";
-
-        calendarString += "\n";
-
-        //calendarString += "Date: " + calendar.get(Calendar.DATE) + "\n";
-
-        return calendarString;
-    }
-
-
-    public Calendar getOffsetCalendar(Calendar originalCalendar, int lowerBound, int upperBound, int multiplier) {
-        Calendar offsetCalendar = (Calendar) originalCalendar.clone();
-
-        int randomDayOffset = randInt(lowerBound, upperBound);
-
-        addDaysToCalendar(offsetCalendar, randomDayOffset*multiplier);
-
-        return offsetCalendar;
     }
 
 }
